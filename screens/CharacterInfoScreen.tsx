1efa68e7784a102ccb8b5467a3198b57e41b2c39
Tobias Dummschat <tdu@trifork.com>
2021-10-12 16:37:32 +0200

    More stuff on character info page

--- screens/CharacterInfoScreen.tsx
+++ screens/CharacterInfoScreen.tsx
@@ -1,36 +1,78 @@
 import * as React from 'react';
 import {Image, ScrollView, StyleSheet} from 'react-native';
 import {Text, View} from '../components/Themed';
-import {RootStackScreenProps} from '../types';
+import {Character, RootStackScreenProps} from '../types';
 
 export default function CharacterInfoScreen({route}: RootStackScreenProps<'CharacterInfo'>) {
+    const character = route.params.character;
+
+
     return (
         <ScrollView>
             <View style={styles.container}>
-                <Text style={styles.title}>
-                    {route.params.character.name}
-                </Text>
+                <View style={styles.container}>
+                    <Text style={styles.title}>
+                        {character.name}
+                    </Text>
+                    <Text style={{color: 'grey'}}>
+                        ID: {character.id}
+                    </Text>
+                </View>
                 <Image
-                    source={{uri: route.params.character.thumbnail.path + "/standard_fantastic." + route.params.character.thumbnail.extension}}
+                    source={{uri: character.thumbnail.path + "/standard_fantastic." + character.thumbnail.extension}}
                     style={{width: 250, height: 250}}
                 />
-                <Text>
-                    {route.params.character.description}
+                <View style={styles.collections}>
+                    {getCollectionView(character, 'comics')}
+                    {getCollectionView(character, 'series')}
+                    {getCollectionView(character, 'stories')}
+                    {getCollectionView(character, 'events')}
+                </View>
+                <Text style={styles.description}>
+                    {character.description}
+                </Text>
+                <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)"/>
+                <Text style={{color: 'grey'}}>
+                    {JSON.stringify(character, null, 4)}
                 </Text>
             </View>
         </ScrollView>
     );
 }
 
+function getCollectionView(character: Character, type: 'comics' | 'series' | 'stories' | 'events') {
+    return <View style={styles.collection}>
+        <Text>
+            {type[0].toUpperCase() + type.substring(1)}
+        </Text>
+        <Text>
+            {character[type].available}
+        </Text>
+    </View>;
+}
+
 const styles = StyleSheet.create({
+    collection: {
+        alignItems: "center",
+        marginHorizontal: 10
+    },
+    collections: {
+        flex: 1,
+        flexDirection: 'row',
+        marginVertical: 30,
+    },
     container: {
         flex: 1,
         flexDirection: 'column',
         alignItems: 'center',
     },
+    description: {
+        marginHorizontal: 10,
+    },
     title: {
         fontSize: 20,
         fontWeight: 'bold',
+        marginTop: 30,
     },
     separator: {
         marginVertical: 30,
