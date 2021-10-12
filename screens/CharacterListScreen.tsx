0fd0b1028eef722f1a8cad41f9907da93457d294
Tobias Dummschat <tdu@trifork.com>
2021-10-12 15:53:57 +0200

    Very basic caching

--- screens/CharacterListScreen.tsx
+++ screens/CharacterListScreen.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {Dimensions, NativeScrollEvent, ScrollView, StyleSheet} from 'react-native';
+import {NativeScrollEvent, ScrollView, StyleSheet} from 'react-native';
 import {View} from '../components/Themed';
 import {Character, RootTabScreenProps} from '../types';
 import CharacterItem from "../components/CharacterItem";
@@ -7,10 +7,12 @@ import MD5 from "crypto-js/md5";
 import debounce from "lodash/debounce";
 import {privateKey, publicKey} from '../apiconfig.json';
 import SearchBar from 'react-native-dynamic-search-bar';
+import * as FileSystem from 'expo-file-system';
 
 export default function CharacterListScreen(navigation: RootTabScreenProps<'Characters'>) {
     let [characters, setCharacters] = React.useState<Character[]>([]);
     let [currentSearchPrefix, setCurrentSearchPrefix] = React.useState<string>('');
+    const cacheUri = FileSystem.cacheDirectory + "characters.json";
 
     let fetchCharacters = debounce(async (nameStartsWith: string) => {
         const clearCharacters = nameStartsWith !== currentSearchPrefix;
@@ -23,7 +25,8 @@ export default function CharacterListScreen(navigation: RootTabScreenProps<'Char
             apikey: publicKey,
             hash: hash,
             offset: clearCharacters ? 0 : characters.length,
-            nameStartsWith: nameStartsWith
+            nameStartsWith: nameStartsWith,
+            limit: 100
         }
         const urlWithParams = url + '?' +
             Object.entries(params)
@@ -40,15 +43,19 @@ export default function CharacterListScreen(navigation: RootTabScreenProps<'Char
         }).then(r => {
             if (r.status == 200) {
                 r.json().then(body => {
+                    let newCharacters: Character[];
                     if (clearCharacters) {
-                        setCharacters(body.data.results);
+                        newCharacters = body.data.results;
                         setCurrentSearchPrefix(nameStartsWith);
                     } else {
                         const responseCharacters: Character[] = body.data.results;
                         const currentCharacterIds: number[] = characters.map(c => c.id);
                         const addedCharacters: Character[] = responseCharacters.filter(c => !(c.id in currentCharacterIds))
-                        setCharacters(characters.concat(addedCharacters));
+                        newCharacters = characters.concat(addedCharacters);
                     }
+                    setCharacters(newCharacters);
+                    if (nameStartsWith === '')
+                        persistCharacters(newCharacters);
                 });
             } else {
                 r.json().then(body => alert(JSON.stringify(body)));
@@ -56,8 +63,22 @@ export default function CharacterListScreen(navigation: RootTabScreenProps<'Char
         })
     }, 500, {leading: true});
 
+    const persistCharacters = async (charactersToCache: Character[]) => {
+        await FileSystem.writeAsStringAsync(cacheUri, JSON.stringify(charactersToCache));
+    }
+
+    const initializeCharacters = async () => {
+        const characterJson = await FileSystem.readAsStringAsync(cacheUri);
+        if (characterJson !== '') {
+            const charactersFromCache: Character[] = JSON.parse(characterJson);
+            setCharacters(charactersFromCache);
+        } else {
+            fetchCharacters('');
+        }
+    }
+
     React.useEffect(() => {
-        fetchCharacters('');
+        initializeCharacters().then();
     }, []);
 
     return (
@@ -84,7 +105,7 @@ export default function CharacterListScreen(navigation: RootTabScreenProps<'Char
 }
 
 const isCloseToBottom = ({layoutMeasurement, contentOffset, contentSize}: NativeScrollEvent) => {
-    const paddingToBottom = Dimensions.get("screen").height;
+    const paddingToBottom = 1000;
     return layoutMeasurement.height + contentOffset.y >=
         contentSize.height - paddingToBottom;
 };
